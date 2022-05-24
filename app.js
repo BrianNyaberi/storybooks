c3b8b37ee571f0f1d2ae698e1443b0b204f08d7c
Web worker <briannyaberi1@gmail.com>
2022-05-24 14:58:42 +0300

    morgan middleware setup

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const morgan = require('morgan')
 const connectDB = require('./config/db')
 
 // Load the config file
 dotenv.config({path:'./config/config.env'})
 
-connectDB() 
+connectDB()
+
 const app = express()
 
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`))
