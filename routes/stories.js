1915faeb85e2fb8734a873431b5cf2f32ab1521a
Web worker <briannyaberi1@gmail.com>
2022-06-06 22:34:28 +0300

    Post stories

--- routes/stories.js
+++ routes/stories.js
@@ -10,4 +10,17 @@ router.get('/add', ensureAuth, (req, res) => {
     res.render('stories/add')
 })
 
+// @desc Add stories to db
+// @route POST /stories
+router.get('/', ensureAuth, async (req, res) => {
+    try {
+        req.body.user = req.user.id
+        await Story.create(req.body)
+        res.redirect('/dasboard')
+    } catch (err) {
+        console.error(err)
+        res.render('error/500')
+    }
+})
+
 module.exports = router
